6cb35a7ff36335e8f12c4f53cd92a1904d9dac88
gedemartha <alphamart12@gmail.com>
2025-03-05 11:28:37 +0800

    password to 6 letters only

--- lib/zod.ts
+++ lib/zod.ts
@@ -3,7 +3,7 @@ import { object, string } from "zod";
 export const SignInSchema = object({
   email: string().email("Invalid Email"),
   password: string()
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 6 characters")
     .max(32, "Password must be less than 32 characters"),
 });
 
@@ -12,10 +12,10 @@ export const RegisterSchema = object({
   username: string().min(1, "Username must be more than 1 character"),
   email: string().email("Invalid Email"),
   password: string()
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 6 characters")
     .max(32, "Password must be less than 32 characters"),
   ConfirmPassword: string()
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 6 characters")
     .max(32, "Password must be less than 32 characters"),
 }).refine((data) => data.password === data.ConfirmPassword, {
   message: "| Password does not match",
@@ -34,6 +34,6 @@ export const UserSchema = object({
   username: string().min(1, "Username must be more than 1 character"),
   email: string().email("Invalid Email"),
   password: string()
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 8 characters")
     .max(32, "Password must be less than 32 characters"),
 });
